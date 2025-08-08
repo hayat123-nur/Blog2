cac769175c52c7f2fd95051fb5dadd10f3bf8c7f
hayat123-nur <nurahayat4@gmail.com>
2025-08-08 17:18:54 +0300

    Initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import BlogCard from "../components/BlogCard";
+import { blogData } from "../data/constant";
 import { Button } from "@/components/ui/button";
-
 
 const Home = () => {
   return (
-    <div>
+    <>
       <div className="relative ">
         <img src="/hero.png" alt="" className="w-full h-full object-cover" />
         <img
@@ -14,13 +14,18 @@ const Home = () => {
                      w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24"
         />
       </div>
-      <h2 className="ml-10 ">Related Blog</h2>
-      <BlogCard />
-
-      <div className="flex justify-center m-10">
-        <Button>Show more</Button>
-      </div>
-    </div>
+      <h1 className="text-2xl p-5">Recent Blog Posts</h1>
+      <section className="px-6 md:px-10 py-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-2 py-4">
+          {blogData.map((blog) => (
+            <BlogCard key={blog.id} blog={blog} />
+          ))}
+        </div>
+        <div className="flex justify-center m-10">
+          <Button>Show more</Button>
+        </div>
+      </section>
+    </>
   );
 };
 
